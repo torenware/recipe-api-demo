ce16fe859f1c4c262fc7b96841a798b421724a97
Rob Thorne <rob@torenware.com>
2020-08-13 15:25:56 -0700

    Added validations and superuser support

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
+from django.core.validators import validate_email
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a new user"""
+
+        if not email:
+            raise ValueError('Email field is required')
+
+        validate_email(email)
+
         user = self.model(
             email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
@@ -14,10 +21,24 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password, **extra_fields):
+        """Allow for creation of admin users"""
+
+        # create a plain user
+        user = self.create_user(email, password)
+
+        # And bless it into adminhood
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports email as username"""
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(
+        max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 
 class ModelTests(TestCase):
@@ -27,3 +28,28 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, "test@django-course.com")
+
+    def test_user_has_email(self):
+        """Test if lacking an email throws an exception"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_email_must_be_valid(self):
+        """New user must be passed syntactically valid email"""
+
+        email = 'not an email'
+
+        with self.assertRaises(ValidationError):
+            get_user_model().objects.create_user(email, 'test123')
+
+    def test_admin_user_creates(self):
+        """Test creation of an admin user"""
+
+        user = get_user_model().objects.create_superuser(
+            "super@user.com",
+            "pass-a-woid"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
