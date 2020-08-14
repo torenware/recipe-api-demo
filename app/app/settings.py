542841f438c0020d260518ec480dbdd1232219c1
Rob Thorne <rob@torenware.com>
2020-08-13 20:56:08 -0700

    Attempt to use .env file for secrets

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#98r_(y+$s8z6##v#gv35i_un76@_wxvsf251l8g2+dmjt&qq('
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
