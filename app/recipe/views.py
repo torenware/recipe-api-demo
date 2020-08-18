66a76b913944c5ac957e8b8eb490d811a3c22112
Rob Thorne <rob@torenware.com>
2020-08-17 18:08:31 -0700

    Added image uploading.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -6,7 +8,7 @@ from core.models import Tag, Ingredient, Recipe
 from recipe.serializers \
     import \
     TagSerializer, IngredientSerializer, RecipeSerializer, \
-    RecipeDetailSerializer
+    RecipeDetailSerializer, RecipeImageSerializer
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -59,9 +61,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return different serializer for our detail view"""
         if self.action == 'retrieve':
             return RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Override to make sure a new attribute belongs to its user"""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        # And if not...
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
